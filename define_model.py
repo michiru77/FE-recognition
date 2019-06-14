d034e1f364adaf5227cc259cd6d7f255de0caf69
minemichi <e155736@ie.u-ryukyu.ac.jp>
2019-06-14 16:42:23 +0900

    ver1.0

--- define_model.py
+++ define_model.py
@@ -10,6 +10,7 @@ from keras.optimizers import Adagrad
 class CNN_model:
     def __init__(self, arg_label_num, arg_size):
         self.class_num = arg_label_num
+        self.binary_class_num = 2
         self.size = arg_size
         
     def nisime_kai_gap_model(self):
@@ -43,6 +44,42 @@ class CNN_model:
         # 23
         model.add(GlobalAveragePooling2D())
         model.add(Dense(self.class_num))
+        model.add(Activation('softmax'))
+        # compile
+        model.compile(loss="categorical_crossentropy", optimizer=Adagrad(), metrics=["accuracy"])
+        return model
+
+    def nisime_kai_gap_binary_model(self):
+        model = Sequential()
+        # 0~6
+        model.add(Conv2D(64, (3, 3), padding='valid', data_format="channels_last", input_shape=(self.size, self.size, 1)))
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        # 7~15
+        model.add(Conv2D(128, (3, 3), padding='valid'))
+        model.add(Activation('relu'))
+        model.add(Conv2D(128, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(Conv2D(128, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        # 16~22
+        model.add(Conv2D(256, (3, 3), padding='valid'))
+        model.add(Activation('relu'))
+        model.add(Conv2D(256, (3, 3)))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.5))
+        # 23
+        model.add(GlobalAveragePooling2D())
+        model.add(Dense(self.binary_class_num))
         model.add(Activation('softmax'))
         # compile
         model.compile(loss="categorical_crossentropy", optimizer=Adagrad(), metrics=["accuracy"])
